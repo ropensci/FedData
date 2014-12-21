ead7713bf34e098402f6a33d29d621c43cc19471
R. Kyle Bocinsky <bocinsky@users.noreply.github.com>
2014-12-21 11:30:17 -0800

    Made the getGHCN function grab monthly data

--- R/GHCN_FUNCTIONS.R
+++ R/GHCN_FUNCTIONS.R
@@ -1,3 +1,5 @@
+pkgTest("zoo")
+
 downloadGHCN <- function(ID, data.dir="../DATA/"){
   if(!file.exists(data.dir)){
     dir.create(data.dir, recursive=T)
@@ -21,7 +23,7 @@ getGHCN <- function(ID, data.dir="../DATA/", element, months=c(1:12), fun){
     }
   }
   
-  series <- mclapply(ID,FUN=function(x,...){extractGHCNDaily(x, data.dir=data.dir, element=element, months=months, fun=fun)}, mc.cores=detectCores())
+  series <- mclapply(ID,FUN=function(x,...){extractGHCNMonthly(x, data.dir=data.dir, element=element, months=months, fun=fun)}, mc.cores=detectCores())
   
   # convert to list of zoo objects
   series <- lapply(series, function(x) zoo(as.matrix(x[ ,-1, drop = FALSE]),as.character(x[,1])))
@@ -36,7 +38,7 @@ getGHCN <- function(ID, data.dir="../DATA/", element, months=c(1:12), fun){
   
 }
 
-extractGHCNDaily <- function(id, data.dir="../DATA/", element, months=c(1:12), fun=NULL){
+extractGHCNDaily <- function(id, data.dir="../DATA/", element){
   # If more months than a year, break
   if(length(months)>12){
     stop("ERROR! Too many months.")
@@ -51,23 +53,58 @@ extractGHCNDaily <- function(id, data.dir="../DATA/", element, months=c(1:12), f
   names(daily)[1:4] <- c("STATION","YEAR","MONTH","ELEMENT")
   daily <- daily[daily$ELEMENT==toupper(element),c(2:3,seq(5,125,4))]
   daily[daily==-9999] <- NA
-#   if(fun=="sum"){
-#     daily <- cbind(daily[,1:2],AGG=rowSums(daily[,3:33], na.rm=T))
-#   }else if(fun=="mean"){
-#     daily <- cbind(daily[,1:2],AGG=rowMeans(daily[,3:33], na.rm=T))
-#   }
-#   daily$YEAR[daily$MONTH %in% previous.year] <- daily$YEAR[daily$MONTH %in% previous.year]+1
-#   daily <- daily[!(daily$MONTH %in% (1:12)[!(1:12 %in% c(previous.year,current.year,next.year))]),]
-#   daily <- daily[complete.cases(daily),]
-#   if(fun=="sum"){
-#     monthly <- aggregate(daily, by=list(daily$YEAR), FUN=sum)[,c(1,4)]
-#   }else if(fun=="mean"){
-#     monthly <- aggregate(daily, by=list(daily$YEAR), FUN=mean)[,c(1,4)]
-#   }
-#   names(monthly) <- c("YEAR",id)
+  #   if(fun=="sum"){
+  #     daily <- cbind(daily[,1:2],AGG=rowSums(daily[,3:33], na.rm=T))
+  #   }else if(fun=="mean"){
+  #     daily <- cbind(daily[,1:2],AGG=rowMeans(daily[,3:33], na.rm=T))
+  #   }
+  #   daily$YEAR[daily$MONTH %in% previous.year] <- daily$YEAR[daily$MONTH %in% previous.year]+1
+  #   daily <- daily[!(daily$MONTH %in% (1:12)[!(1:12 %in% c(previous.year,current.year,next.year))]),]
+  #   daily <- daily[complete.cases(daily),]
+  #   if(fun=="sum"){
+  #     monthly <- aggregate(daily, by=list(daily$YEAR), FUN=sum)[,c(1,4)]
+  #   }else if(fun=="mean"){
+  #     monthly <- aggregate(daily, by=list(daily$YEAR), FUN=mean)[,c(1,4)]
+  #   }
+  #   names(monthly) <- c("YEAR",id)
   
   return(daily)
 }
+
+extractGHCNMonthly <- function(id, data.dir="../DATA/", element, months=c(1:12), fun=NULL){
+  # If more months than a year, break
+  if(length(months)>12){
+    stop("ERROR! Too many months.")
+  }
+  
+  # Process the months to get months from current, previous, and future years
+  previous.year <- 12+months[months<1]
+  current.year <- months[months>=1 & months<=12]
+  next.year <- months[months>12]-12
+  
+  daily <- read.fwf(paste(data.dir,id,".dly",sep=''),c(11,4,2,4,rep(c(5,1,1,1),31)))
+  names(daily)[1:4] <- c("STATION","YEAR","MONTH","ELEMENT")
+  daily <- daily[daily$ELEMENT==toupper(element),c(2:3,seq(5,125,4))]
+  daily[daily==-9999] <- NA
+  
+  if(fun=="sum"){
+    daily <- cbind(daily[,1:2],AGG=rowSums(daily[,3:33], na.rm=T))
+  }else if(fun=="mean"){
+    daily <- cbind(daily[,1:2],AGG=rowMeans(daily[,3:33], na.rm=T))
+  }
+  daily$YEAR[daily$MONTH %in% previous.year] <- daily$YEAR[daily$MONTH %in% previous.year]+1
+  daily <- daily[!(daily$MONTH %in% (1:12)[!(1:12 %in% c(previous.year,current.year,next.year))]),]
+  daily <- daily[complete.cases(daily),]
+  if(fun=="sum"){
+    monthly <- aggregate(daily, by=list(daily$YEAR), FUN=sum)[,c(1,4)]
+  }else if(fun=="mean"){
+    monthly <- aggregate(daily, by=list(daily$YEAR), FUN=mean)[,c(1,4)]
+  }
+  names(monthly) <- c("YEAR",id)
+  
+  return(monthly)
+}
+
 
 getGHCNStationMetadataSPDF <- function(station.names=NULL, data.dir="../DATA/", write.output=T){
   if(!file.exists(paste(data.dir,"ghcnd-stations.txt",sep=''))){
