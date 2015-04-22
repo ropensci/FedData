5f6de2d7ae42dc4579463b7225532f0aab6efda9
Bocinsky <Bocinsky@gmail.com>
2015-04-22 13:57:36 -0700

    updated NHD datasets

--- inst/FedDataTester.R
+++ inst/FedDataTester.R
@@ -31,6 +31,16 @@ NHD <- getNHD(template=county, label=paste(county$STATE,'_',county$NAME, sep='')
 # Get the NRCS SSURGO data (USA ONLY)
 SSURGO <- getSSURGO(template=county, label=paste(county$STATE,'_',county$NAME, sep=''))
 
+# Get the ITRDB records
+itrdb.data <- getITRDB(recon.years=1:2000, 
+                       calib.years=1924:1983, 
+                       species=NULL, 
+                       measurement.type="Ring Width", 
+                       chronology.type="Standard", 
+                       makeSpatial=T)
+
+
+
 slope <- terrain(NED, opt='slope')
 aspect <- terrain(NED, opt='aspect')
 NED.hill <- hillShade(slope, aspect, 40, 230)
@@ -48,42 +58,3 @@ plot(GHCN.prcp[[1]], pch=17, add=T)
 plot(GHCN.temp[[1]], pch=19, add=T)
 
 # plot(SSURGO[['spatial']])
-
-wine <- merge(SSURGO[['spatial']],SSURGO[['tabular']]$mucropyld[SSURGO[['tabular']]$mucropyld$cropname=='Wine grapes',],by.x='MUKEY',by.y="mukey",all=T)[,c("nonirryield.r","irryield.r")]
-wine$yield.max <- pmax(wine$irryield.r,wine$nonirryield.r,na.rm=T)
-
-
-
-
-# Plot NED
-colors <- paste0(colorRampPalette(brewer.pal(9, "Greens"),bias=2)(1000),"CC")
-plot(county)
-plot(NED.hill, col=grey(30:100/100), maxpixels=2000000, useRaster=T, legend=FALSE,  xlab="", ylab="", axes=FALSE, main='', add=T)
-plot(NED, col=colors, maxpixels=2000000, useRaster=T, legend=FALSE,  xlab="", ylab="", axes=FALSE, main='', add=T)
-plot(county, add=T)
-
-# Plot NHD
-plot(NHD$NHDFlowline)
-sp::plot(NHD$NHDWaterbody, col='black', add=T)
-sp::plot(NHD$NHDArea, col='black', add=T)
-
-# Plot average potential wine grape yield
-colors <- paste0(colorRampPalette(brewer.pal(9, "Reds"))(1000),"CC")
-wine$colors <- colors[wine$yield.max*100]
-plot(county)
-plot(NED.hill, col=grey(70:100/100), maxpixels=2000000, useRaster=T, legend=FALSE,  xlab="", ylab="", axes=FALSE, main='', add=T)
-sp::plot(wine, col=wine$colors,border=NA, add=T)
-plot(county, add=T)
-
-
-
-
-
-itrdb.data <- getITRDB(recon.years=1:2000, 
-                       calib.years=1924:1983, 
-                       species=NULL, 
-                       measurement.type="Ring Width", 
-                       chronology.type="Standard", 
-                       makeSpatial=T)
-
-
