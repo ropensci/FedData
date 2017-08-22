88218aadda7d3d5756c67b8aa4fa44ca9aa9662d
Bocinsky <bocinsky@gmail.com>
2017-08-22 13:25:30 -0600

    Forcing Travis re-run

--- tests/testthat/test.DAYMET.R
+++ tests/testthat/test.DAYMET.R
@@ -6,10 +6,10 @@ test_that("The Daymet tiles are available at the correct URL", {
   year <- 1985
   element <- "prcp"
   tileID <- 11376
-  url <- paste0('http://thredds.daac.ornl.gov/thredds/fileServer/ornldaac/1328/tiles/',year,'/',tileID,'_',year,'/',element,'.nc')
+  url <- paste0('https://thredds.daac.ornl.gov/thredds/fileServer/ornldaac/1328/tiles/',year,'/',tileID,'_',year,'/',element,'.nc')
   expect_false(suppressWarnings(httr::http_error(url)))
   
   tileID <- 0
-  url <- paste0('http://thredds.daac.ornl.gov/thredds/fileServer/ornldaac/1328/tiles/',year,'/',tileID,'_',year,'/',element,'.nc')
+  url <- paste0('https://thredds.daac.ornl.gov/thredds/fileServer/ornldaac/1328/tiles/',year,'/',tileID,'_',year,'/',element,'.nc')
   expect_true(suppressWarnings(httr::http_error(url)))
 })
