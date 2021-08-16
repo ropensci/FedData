f649a90f3c8c06a1217ec7bf18f33ce8fbe5db89
Jeff Hollister <jeff.w.hollister@gmail.com>
2021-08-16 16:17:30 -0400

    Caputred and suppressed s2 cat statements

--- R/SSURGO_FUNCTIONS.R
+++ R/SSURGO_FUNCTIONS.R
@@ -385,7 +385,7 @@ get_ssurgo_study_area <- function(template = NULL, area, date, raw.dir) {
 
   utils::unzip(file, exdir = tmpdir)
   suppressMessages({
-    if(sf::sf_use_s2()){sf::sf_use_s2(FALSE)}
+    if(sf::sf_use_s2()){invisible(capture.output(sf::sf_use_s2(FALSE)))}
     mapunits <-
       sf::read_sf(paste0(tmpdir, "/", area, "/spatial"),
         layer = paste0("soilmu_a_", tolower(area))
@@ -393,7 +393,7 @@ get_ssurgo_study_area <- function(template = NULL, area, date, raw.dir) {
       sf::st_make_valid() %>%
       dplyr::group_by(AREASYMBOL, SPATIALVER, MUSYM, MUKEY) %>%
       dplyr::summarise()
-    if(!sf::sf_use_s2()){sf::sf_use_s2(TRUE)}
+    if(!sf::sf_use_s2()){invisible(capture.output(sf::sf_use_s2(TRUE)))}
   })
 
   # Read in all tables
