aa29f00ee0c554772f7b073a20916eccf16d71ae
Kyle Bocinsky <bocinsky@gmail.com>
2023-09-21 14:13:28 -0600

    trying to fix missing GHCN data

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onAttach <- function(libname, pkgname) {
   msg <-
-    "You have loaded FedData v4.0.0,
-the latest major version of FedData. We have retired
-FedData dependencies on the `sp` and `raster` packages.
+    "You have loaded FedData v4.
+As of FedData v4 we have retired
+dependencies on the `sp` and `raster` packages.
 All functions in FedData v4 return `terra` (raster)
 or `sf` (vector) objects by default, and there may be
 other breaking changes."
